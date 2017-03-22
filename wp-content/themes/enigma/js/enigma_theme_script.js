6d4d3e66367e2dedf047512b1f3cc667baa42468
Matthew J. Sahagian <matt@imarc.com>
2017-03-22 16:45:37 -0700

    Open all external links in new tab

--- wp-content/themes/enigma/js/enigma_theme_script.js
+++ wp-content/themes/enigma/js/enigma_theme_script.js
@@ -1,18 +1,32 @@
  //enigma  social tooltip js
  jQuery(function(){
-		jQuery('li').tooltip();
-		jQuery("[data-toggle='tooltip']").tooltip();
-		jQuery("[data-toggle='popover']").popover();
-		
-		
-		
-    });
+	jQuery('li').tooltip();
+	jQuery("[data-toggle='tooltip']").tooltip();
+	jQuery("[data-toggle='popover']").popover();
+
+
 
 	/*----------------------------------------------------*/
+	/*	Open external links in new window
+	/*----------------------------------------------------*/
+
+	jQuery('a').each(function() {
+		var a = new RegExp('/' + window.location.host + '/');
+		if(!a.test(this.href)) {
+			jQuery(this).click(function(event) {
+				event.preventDefault();
+				event.stopPropagation();
+				window.open(this.href, '_blank');
+			});
+		}
+	});
+});
+
+/*----------------------------------------------------*/
 /*	Scroll To Top Section
 /*----------------------------------------------------*/
 	jQuery(document).ready(function () {
-	
+
 		jQuery(window).scroll(function () {
 			if (jQuery(this).scrollTop() > 100) {
 				jQuery('.enigma_scrollup').fadeIn();
@@ -20,17 +34,17 @@
 				jQuery('.enigma_scrollup').fadeOut();
 			}
 		});
-	
+
 		jQuery('.enigma_scrollup').click(function () {
 			jQuery("html, body").animate({
 				scrollTop: 0
 			}, 600);
 			return false;
 		});
-	
-	});	
 
-	
+	});
+
+
 	jQuery.browser = {};
 			(function () {
 				jQuery.browser.msie = false;
