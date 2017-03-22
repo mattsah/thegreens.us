e24850ed1307b9de4f0ff77c7ac37b4cec086631
Matthew J. Sahagian <matt@imarc.com>
2017-03-22 16:31:54 -0700

    New contact icons + email signup in header

--- wp-content/themes/greenigma/header.php
+++ wp-content/themes/greenigma/header.php
@@ -9,11 +9,11 @@
 <html <?php language_attributes(); ?>><!--<![endif]-->
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>" charset="<?php bloginfo('charset'); ?>" />
 	<?php $wl_theme_options = weblizar_get_options(); ?>
 	<?php if($wl_theme_options['upload_image_favicon']!=''){ ?>
-	<link rel="shortcut icon" href="<?php  echo esc_url($wl_theme_options['upload_image_favicon']); ?>" /> 
+	<link rel="shortcut icon" href="<?php  echo esc_url($wl_theme_options['upload_image_favicon']); ?>" />
 	<?php } ?>
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
 	<?php wp_head(); ?>
@@ -37,14 +37,14 @@
 		<div class="container headerContainer" >
 			<!-- Logo & Contact Info -->
 			<div class="row ">
-				<div class="col-md-6 col-sm-12 titleContainer">					
-					<div claSS="logo">						
+				<div class="col-md-6 col-sm-12 titleContainer">
+					<div claSS="logo">
 					<a href="<?php echo esc_url(home_url( '/' )); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-					<?php 
-					if($wl_theme_options['upload_image_logo']!='') 
+					<?php
+					if($wl_theme_options['upload_image_logo']!='')
 					{ ?>
 					<img src="<?php echo esc_url($wl_theme_options['upload_image_logo']); ?>" style="height:<?php if($wl_theme_options['height']!='') { echo $wl_theme_options['height']; }  else { "80"; } ?>px; width:<?php if($wl_theme_options['width']!='') { echo $wl_theme_options['width']; }  else { "200"; } ?>px;" />
-					<?php } else { ?> 
+					<?php } else { ?>
 					<?php echo esc_attr_e('Greenigma',"weblizar"); ?>
 					<?php } ?>
 					</a>
@@ -57,14 +57,12 @@
 				</div>
 				<?php if($wl_theme_options['header_social_media_in_enabled']=='1') { ?>
 				<div class="col-md-6 col-sm-12">
-				<?php if($wl_theme_options['email_id'] || $wl_theme_options['phone_no'] !='') { ?>
-				<ul class="head-contact-info">
-						<?php if($wl_theme_options['email_id'] !='') { ?><li><i class="fa fa-envelope"></i><a href="mailto:<?php echo $wl_theme_options['email_id']; ?>"><?php echo esc_attr($wl_theme_options['email_id']); ?></a></li><?php } ?>
-						<?php if($wl_theme_options['phone_no'] !='') { ?><li><i class="fa fa-phone"></i><?php echo esc_attr($wl_theme_options['phone_no']); ?></li><?php } ?>
-				</ul>
-				<?php } ?>
 					<ul class="social">
-					<?php if($wl_theme_options['fb_link']!='') { ?>
+					<?php if($wl_theme_options['email_id']!='') { ?>
+						<li class="email" data-toggle="tooltip" data-placement="bottom" title="Email"><a  href="mailto:<?php echo esc_attr($wl_theme_options['email_id']); ?>" target="_blank"><i class="fa fa-envelope"></i></a></li>
+					<?php } if($wl_theme_options['phone_no']!='') { ?>
+						<li class="phone" data-toggle="tooltip" data-placement="bottom" title="Telephone"><a  href="tel:<?php echo esc_attr($wl_theme_options['phone_no']); ?>" target="_blank"><i class="fa fa-phone"></i></a></li>
+					<?php } if($wl_theme_options['fb_link']!='') { ?>
 					   <li class="facebook" data-toggle="tooltip" data-placement="bottom" title="Facebook"><a  href="<?php echo esc_url($wl_theme_options['fb_link']); ?>" target="_blank"><i class="fa fa-facebook"></i></a></li>
 					<?php } if($wl_theme_options['twitter_link']!='') { ?>
 					<li class="twitter" data-toggle="tooltip" data-placement="bottom" title="Twitter"><a href="<?php echo esc_url($wl_theme_options['twitter_link']); ?>" target="_blank"><i class="fa fa-twitter"></i></a></li>
@@ -75,13 +73,17 @@
 					<?php } if($wl_theme_options['youtube_link']!='') { ?>
 					<li class="youtube" data-toggle="tooltip" data-placement="bottom" title="Youtube"><a href="<?php echo esc_url($wl_theme_options['youtube_link']) ; ?>" target="_blank"><i class="fa fa-youtube"></i></a></li>
 	                <?php } ?>
-					</ul>	
+					</ul>
+					<form class="signup" action="//thegreens.us15.list-manage.com/subscribe/post?u=d8925995a05f1e7da88de37c6&amp;id=bd49f4e2f1" method="post">
+						<input type="email" name="EMAIL" placeholder="e.g. user@example.com" required />
+						<button type="submit">Signup</button>
+					</form>
 				</div>
 				<?php } ?>
 			</div>
 			<!-- /Logo & Contact Info -->
-		</div>	
-	</div>	
+		</div>
+	</div>
 	<!-- /Header Section -->
 	<!-- Navigation  menus -->
 	<div class="navigation_menu "  data-spy="affix" data-offset-top="95" id="enigma_nav_top">
@@ -90,22 +92,22 @@
 			<nav class="navbar navbar-default " role="navigation">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
-					 
+
 					  <span class="sr-only"><?php __('Toggle navigation',"weblizar"); ?></span>
 					  <span class="icon-bar"></span>
 					  <span class="icon-bar"></span>
 					  <span class="icon-bar"></span>
 					</button>
 				</div>
-				<div id="menu" class="collapse navbar-collapse ">	
+				<div id="menu" class="collapse navbar-collapse ">
 				<?php wp_nav_menu( array(
 						'theme_location' => 'primary',
 						'menu_class' => 'nav navbar-nav',
 						'fallback_cb' => 'weblizar_fallback_page_menu',
 						'walker' => new weblizar_nav_walker(),
 						)
-						);	?>				
-				</div>	
+						);	?>
+				</div>
 			</nav>
 		</div>
 	</div>
